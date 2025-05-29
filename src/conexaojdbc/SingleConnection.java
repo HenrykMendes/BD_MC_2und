59475fe8ac0417441c4c126e61d3b0e373bf06c4
lbarreto-tech <lucaslbcs2003@gmail.com>
2025-05-29 17:31:22 -0300

    refatoracao do SingleConnection

--- src/conexaojdbc/SingleConnection.java
+++ src/conexaojdbc/SingleConnection.java
@@ -1,21 +1,44 @@
 package conexaojdbc;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.DriverManager;
 
 public class SingleConnection {
 	
-	private static Connection connection = null;
+	private static final String url = ${DB_URL};
+	private static final String user = ${DB_USER}; 
+	private static final String password = ${DB_PASSWORD};
+	private static Connection connection;
 	
 	public SingleConnection(String url, String user, String password) {
 		conectar(url, user, password);
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	static {
+		conectar();
 	}
 
 
 	private static void conectar(String url, String user, String password) {
 		try {
 			if(connection == null) {
-				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+				System.out.println("Conectou ao banco com sucesso!");
+			}
+		}catch(Exception e) {
+			System.out.println("Erro ao se conectar ao banco!");
+			e.printStackTrace();
+		}
+	}
+	
+	private static void conectar() {
+		try {
+			if(connection == null) {
 				connection = DriverManager.getConnection(url, user, password);
 				connection.setAutoCommit(false);
 				System.out.println("Conectou ao banco com sucesso!");
